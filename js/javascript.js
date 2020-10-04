cc41d03ff6779745165473ccd084f28673859cc2
Mariga123 <johnmariga8@gmail.com>
2020-10-04 20:39:38 +0300

    add function to javascript

--- js/javascript.js
+++ js/javascript.js
@@ -24,3 +24,20 @@ var daysOfWeek=[
     "Friday",
     "Saturday"
 ];
+function myCode(){
+    event.preventDefault();
+    var gender = document.getElementById('genders').value;
+    var date = document.getElementById('date').value;
+    date = new Date(date);
+
+    if(gender === 'male'){
+        alert(" Since you were born on " + daysOfTheWeek[date.getDay()] + ',' + " your name is " + maleNames[date.getDay()]);
+    }
+   else if(gender === 'female'){
+        alert(" Since you were born on " + daysOfTheWeek[date.getDay()] + ',' + " your name is " + femaleNames[date.getDay()]);
+    }
+    else{
+        alert("Fill in the date and gender");
+    }
+
+}
